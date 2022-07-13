53fd4f117c54f33ebce1530f73b7e3e44e1bb801
jumahbk <contact@banderjumah.com>
2022-07-14 01:02:23 +0300

    d
    d

--- server.py
+++ server.py
@@ -52,4 +52,4 @@ def hook():
 
 
 if __name__ == "__main__":
-    app.run(port=80, debug=True,host="0.0.0.0", ssl_context=('cert.pem', 'key.pem'))
+    app.run(port=5000, debug=True,host="0.0.0.0", ssl_context=('cert.pem', 'key.pem'))
