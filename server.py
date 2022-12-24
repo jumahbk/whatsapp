c05f13beb2e6a737650b5905dd839338487820ff
jumahbk <32467091+jumahbk@users.noreply.github.com>
2022-12-24 18:45:53 +0300

    d
    d

--- server.py
+++ server.py
@@ -94,7 +94,17 @@ def hook():
                     r = messenger.send_templatev2("appointment_cancel", "966500768855", '[{"type": "body","parameters": [{ "type": "text","text": "'+mobile+'"}]}]', "ar")
 
                     messenger.send_message(f"تم استلام طلبكم بالغاء الموعود  و سيتم التواصل معكم لتاكيد الالغاء", mobile)
+                    
+                if userAnswer == 'لحجز العرض':
 
+                      r = messenger.send_templatev2("offeraccept", "966555862924", '[{"type": "body","parameters": [{ "type": "text","text": "'+mobile+'"}]}]', "ar")
+                      r = messenger.send_templatev2("offeraccept", "966554811828", '[{"type": "body","parameters": [{ "type": "text","text": "'+mobile+'"}]}]', "ar")
+
+                    #  r = messenger.send_templatev2("appointment_request", "966557779388", '[{"type": "body","parameters": [{ "type": "text","text": "'+mobile+'"}]}]', "ar")
+                    #  r = messenger.send_templatev2("appointment_request", "966500768855", '[{"type": "body","parameters": [{ "type": "text","text": "'+mobile+'"}]}]', "ar")
+
+                      messenger.send_message(f"شكراً لقبولكم العرض ، تم اخطار مركز الاتصال و سيتم التواصل معكم في اقرب فرصة ", mobile)
+                
                 elif  userAnswer == "طلب او تعديل موعد":
                       r = messenger.send_templatev2("appointment_request", "966555862924", '[{"type": "body","parameters": [{ "type": "text","text": "'+mobile+'"}]}]', "ar")
                       r = messenger.send_templatev2("appointment_request", "966557779388", '[{"type": "body","parameters": [{ "type": "text","text": "'+mobile+'"}]}]', "ar")
