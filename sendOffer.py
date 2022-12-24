5bebbcbca375ed7ec6bd2af318e2f0a2f9427b62
jumahbk <32467091+jumahbk@users.noreply.github.com>
2022-12-24 18:15:57 +0300

    offersingle

---
+++ sendOffer.py
@@ -0,0 +1,71 @@
+import requests
+from flask import Flask,request,json
+from heyoo import WhatsApp
+from urllib.parse import unquote
+from datetime import datetime
+import urllib.request, json 
+
+response = requests.get('https://www.medartclinics.com/tttt.bin')
+
+days = [
+'الأثنين',
+'الثلاثاء',
+'الاربعاء', 
+'الخميس',
+'الجمعة',
+'السبت',
+'الأحد'
+]
+
+
+keytoken = unquote(requests.get('https://www.medartclinics.com/tttt.bin').text)
+def send_message(message, name, mobile):
+  
+    response = requests.get('https://www.medartclinics.com/tttt.bin')
+    messenger = WhatsApp(keytoken,  phone_number_id='105367738941421')
+    r = messenger.send_templatev2("profiloupdate", mobile, '[{"type": "body","parameters": [{ "type": "text","text": "'+name+'"}]}]', "ar")
+    if "error" in r:
+        print("Error")
+        return 'Failed'
+    else :
+        print(r)
+        d = r['messages'][0]['id']
+        print(d)
+        return d
+    
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def hello():
+    if request.method == 'POST':
+
+       return 'ok'
+    else:
+       date = request.args.get('mobile')
+       name = request.args.get('name')
+
+       mobile ='0555862924'  #request.args.get('pId')
+       if(mobile != None):
+
+
+            payload = {
+                    'aptId': '' +str(id),
+                    'patId': '' + str(pId),
+                    'dateSent': datetime.now().strftime("%Y-%m-%d %I:%M %p"),
+                    'waid': '1',
+                    'mobile': '' + mobile,
+                    
+                    'respond':'0',
+                    'accept':'0'
+                }
+            res = requests.post('http://192.168.2.102/whatsappreminders/isDuplicate', data=payload)
+            d = send_message('test2',keytoken, name, mobile )
+            payload['waid']= d
+         
+
+               
+       return "Done"
+
+if __name__ == '__main__':
+
+    app.run(port=9001, host='0.0.0.0')
