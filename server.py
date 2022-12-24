3ed9712b80186d254a4df8237c8688fe5b306f77
jumahbk <32467091+jumahbk@users.noreply.github.com>
2022-12-24 18:44:33 +0300

    dd
    dd

--- server.py
+++ server.py
@@ -91,20 +91,9 @@ def hook():
                     messenger.send_message(f"تم استلام طلبكم بالغاء الموعد  و سيتم التواصل معكم لتاكيد الالغاء", mobile)
                     r = messenger.send_templatev2("appointment_cancel", "966555862924", '[{"type": "body","parameters": [{ "type": "text","text": "'+mobile+'"}]}]', "ar")
                     r = messenger.send_templatev2("appointment_cancel", "966557779388", '[{"type": "body","parameters": [{ "type": "text","text": "'+mobile+'"}]}]', "ar")
-                    #r = messenger.send_templatev2("appointment_cancel", "966500768855", '[{"type": "body","parameters": [{ "type": "text","text": "'+mobile+'"}]}]', "ar")
+                    r = messenger.send_templatev2("appointment_cancel", "966500768855", '[{"type": "body","parameters": [{ "type": "text","text": "'+mobile+'"}]}]', "ar")
 
-                    messenger.send_message(f"تم استلام طلبكم بالغاء الموعد  و سيتم التواصل معكم لتاكيد الالغاء", mobile)
-             
-                if userAnswer == 'لحجز العرض':
-
-                      r = messenger.send_templatev2("offeraccept", "966555862924", '[{"type": "body","parameters": [{ "type": "text","text": "'+mobile+'"}]}]', "ar")
-                      r = messenger.send_templatev2("offeraccept", "966554811828", '[{"type": "body","parameters": [{ "type": "text","text": "'+mobile+'"}]}]', "ar")
-
-                    #  r = messenger.send_templatev2("appointment_request", "966557779388", '[{"type": "body","parameters": [{ "type": "text","text": "'+mobile+'"}]}]', "ar")
-                    #  r = messenger.send_templatev2("appointment_request", "966500768855", '[{"type": "body","parameters": [{ "type": "text","text": "'+mobile+'"}]}]', "ar")
-
-                      messenger.send_message(f"شكراً لقبولكم العرض ، تم اخطار مركز الاتصال و سيتم التواصل معكم في اقرب فرصة ", mobile)
-
+                    messenger.send_message(f"تم استلام طلبكم بالغاء الموعود  و سيتم التواصل معكم لتاكيد الالغاء", mobile)
 
                 elif  userAnswer == "طلب او تعديل موعد":
                       r = messenger.send_templatev2("appointment_request", "966555862924", '[{"type": "body","parameters": [{ "type": "text","text": "'+mobile+'"}]}]', "ar")
